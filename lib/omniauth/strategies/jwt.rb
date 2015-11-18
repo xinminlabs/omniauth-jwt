85912ed8fb1cc89331f92e4778a8895945953b92
Richard Huang <flyerhzm@gmail.com>
2015-11-18 15:00:15 +0800

    add claims_token option

--- lib/omniauth/strategies/jwt.rb
+++ lib/omniauth/strategies/jwt.rb
@@ -5,11 +5,11 @@ module OmniAuth
   module Strategies
     class JWT
       class ClaimInvalid < StandardError; end
-      
+
       include OmniAuth::Strategy
-      
+
       args [:secret]
-      
+
       option :secret, nil
       option :algorithm, 'HS256'
       option :uid_claim, 'email'
@@ -17,11 +17,12 @@ module OmniAuth
       option :info_map, {"name" => "name", "email" => "email"}
       option :auth_url, nil
       option :valid_within, nil
-      
+      option :claims_token, { "redirect_destination" => "/" }
+
       def request_phase
-        redirect options.auth_url
+        redirect "#{options.auth_url}?claims_token=#{::JWT.encode(options.claims_token, 'RSA_PRIVATE')}"
       end
-      
+
       def decoded
         @decoded ||= ::JWT.decode(request.params['jwt'], options.secret, options.algorithm)
         (options.required_claims || []).each do |field|
@@ -31,19 +32,19 @@ module OmniAuth
         raise ClaimInvalid.new("'iat' timestamp claim is too skewed from present.") if options.valid_within && (Time.now.to_i - @decoded["iat"]).abs > options.valid_within
         @decoded
       end
-      
+
       def callback_phase
         super
       rescue ClaimInvalid => e
         fail! :claim_invalid, e
       end
-      
+
       uid{ decoded[options.uid_claim] }
-      
+
       extra do
         {:raw_info => decoded}
       end
-      
+
       info do
         options.info_map.inject({}) do |h,(k,v)|
           h[k.to_s] = decoded[v.to_s]
@@ -51,7 +52,7 @@ module OmniAuth
         end
       end
     end
-    
+
     class Jwt < JWT; end
   end
 end
