835b420064cf0cf5e12e2b39820c159f8369ff13
Richard Huang <flyerhzm@gmail.com>
2015-11-18 17:55:30 +0800

    get info from access_token

--- lib/omniauth/strategies/jwt.rb
+++ lib/omniauth/strategies/jwt.rb
@@ -25,7 +25,7 @@ module OmniAuth
       end
 
       def decoded
-        @decoded ||= ::JWT.decode(request.params['jwt'], OpenSSL::PKey::RSA.new(options.decode_key), true, algorithm: options.algorithm)
+        @decoded ||= ::JWT.decode(request.params['access_token'], OpenSSL::PKey::RSA.new(options.decode_key), true, algorithm: options.algorithm).first
         (options.required_claims || []).each do |field|
           raise ClaimInvalid.new("Missing required '#{field}' claim.") if !@decoded.key?(field.to_s)
         end
